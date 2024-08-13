892078f7326c77db1a89763d4911f94540e2696d
Abdelrahman Elshreif <link93955@gmail.com>
2024-08-13 05:15:48 +0300

    axios baseURL

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -5,7 +5,8 @@ axios.defaults.withCredentials = true
 // todo: Token from Cookies
 
 // const baseUrl = 'https://api.hazemmahdyd.net/'
-const baseUrl = 'http://localhost:3000/'
+// const baseUrl = 'http://localhost:3000/'
+const baseUrl = 'https://odeaura-api.vercel.app/'
 
 const axiosClient = axios.create({
   baseURL: baseUrl,
