89c5d2ffb760d26b41744c2026c0bd9010036b9a
Abdelrahman Elshreif <link93955@gmail.com>
2024-08-13 05:56:33 +0300

    edit front

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -4,8 +4,8 @@ axios.defaults.withCredentials = true
 
 // todo: Token from Cookies
 
-const baseUrl = 'http://localhost:3000/'
-// const baseUrl = 'https://odeaura-api.vercel.app/'
+// const baseUrl = 'http://localhost:3000/'
+const baseUrl = 'https://odeaura-api.vercel.app/'
 
 const axiosClient = axios.create({
   baseURL: baseUrl,
