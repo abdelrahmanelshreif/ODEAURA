379480a745a0ca0c192fa8ec491d9fda80a5f9b1
Abdelrahman Elshreif <link93955@gmail.com>
2024-08-14 12:37:28 +0300

    edit axiosClient - LoginJSX APPJS AUTHCONT

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -41,21 +41,76 @@
 // )
 // export default axiosClient
 
+
+
+
+
+
+
+
+
+// import axios from 'axios';
+// import queryString from 'query-string';
+// axios.defaults.withCredentials = true;
+
+
+// // Function to get token from cookies
+// const getTokenFromCookies = () => {
+//   const cookies = document.cookie.split(';');
+//   for (let cookie of cookies) {
+//     const [name, value] = cookie.trim().split('=');
+//     if (name === 'token') {
+//       return value;
+//     }
+//   }
+//   return null;
+// };
+
+// const baseUrl = 'https://odeaura-api.vercel.app/';
+// // const baseUrl = 'http://localhost:3000/';
+
+// const axiosClient = axios.create({
+//   baseURL: baseUrl,
+//   headers: {
+//     'content-type': 'application/json',
+//   },
+//   paramsSerializer: (params) => queryString.stringify(params),
+// });
+
+
+// axiosClient.interceptors.request.use(async (config) => {
+//   const token = getTokenFromCookies();
+//   if (token) {
+//     config.headers['Authorization'] = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+
+// axiosClient.interceptors.response.use(
+//   (response) => {
+//     if (response && response.data) {
+//       return response.data;
+//     }
+//     return response;
+//   },
+//   (err) => {
+//     if (!err.response) {
+//       return err;
+//     }
+//     throw err.response;
+//   }
+// );
+
+
+// export default axiosClient;
+
+
+
 import axios from 'axios';
 import queryString from 'query-string';
-axios.defaults.withCredentials = true;
 
-// Function to get token from cookies
-const getTokenFromCookies = () => {
-  const cookies = document.cookie.split(';');
-  for (let cookie of cookies) {
-    const [name, value] = cookie.trim().split('=');
-    if (name === 'token') { // Replace 'token' with the name of your cookie
-      return value;
-    }
-  }
-  return null;
-};
+axios.defaults.withCredentials = true;
 
 const baseUrl = 'https://odeaura-api.vercel.app/';
 // const baseUrl = 'http://localhost:3000/';
@@ -68,15 +123,18 @@ const axiosClient = axios.create({
   paramsSerializer: (params) => queryString.stringify(params),
 });
 
+// Function to get token from local storage
+const getTokenFromLocalStorage = () => {
+  return localStorage.getItem('token'); // Retrieve the token from local storage
+};
 
 axiosClient.interceptors.request.use(async (config) => {
-  const token = getTokenFromCookies();
+  const token = getTokenFromLocalStorage();
   if (token) {
     config.headers['Authorization'] = `Bearer ${token}`;
   }
   return config;
 });
-
 
 axiosClient.interceptors.response.use(
   (response) => {
@@ -92,6 +150,5 @@ axiosClient.interceptors.response.use(
     throw err.response;
   }
 );
-
 
 export default axiosClient;
--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -26,35 +26,70 @@ const signup = async (req, res) => {
   }
 }
 
-const login = async (req, res) => {
-  const { email, password } = req.body
-  try {
-    // login
-    const user = await User.login(email, password)
+// const login = async (req, res) => {
+//   const { email, password } = req.body
+//   try {
+//     // login
+//     const user = await User.login(email, password)
 
-    // create token
-    const payload = { id: user._id, isAdmin: user.isAdmin }
+//     // create token
+//     const payload = { id: user._id, isAdmin: user.isAdmin }
+//     const token = jwt.sign(payload, process.env.SECRET_KEY, {
+//       expiresIn: maxAge,
+//     })
+
+//     res.cookie('jwt', token, {
+//       httpOnly: true,
+//       secure: true, 
+//       sameSite: 'None',
+//       maxAge: maxAge * 1000,
+//     })
+//     delete user._doc.password
+
+//     res.status(200).json(user)
+//   } catch (err) {
+//     const errors = handleLoginError(err)
+//     if (Object.keys(errors).length > 0) return res.status(400).json(errors)
+
+//     console.log(`login post error: ${err}`)
+//     res.status(500).json({ message: 'Internal server error' })
+//   }
+// }
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    // Authenticate user
+    const user = await User.login(email, password); // Ensure this method checks hashed password
+
+    // Create token
+    const payload = { id: user._id, isAdmin: user.isAdmin };
     const token = jwt.sign(payload, process.env.SECRET_KEY, {
       expiresIn: maxAge,
-    })
+    });
 
+    // Optionally set the cookie (if you want to use cookies as well)
     res.cookie('jwt', token, {
       httpOnly: true,
-      secure: true, 
+      secure: process.env.NODE_ENV === 'production', // Set secure based on environment
       sameSite: 'None',
       maxAge: maxAge * 1000,
-    })
-    delete user._doc.password
+    });
 
-    res.status(200).json(user)
+    // Send the token in the JSON response
+    delete user._doc.password; // Remove password before sending user data
+    res.status(200).json({
+      message: 'Login successful',
+      token: token, // Include the token in the response
+      user: user,   // Optionally include user data
+    });
   } catch (err) {
-    const errors = handleLoginError(err)
-    if (Object.keys(errors).length > 0) return res.status(400).json(errors)
+    const errors = handleLoginError(err);
+    if (Object.keys(errors).length > 0) return res.status(400).json(errors);
 
-    console.log(`login post error: ${err}`)
-    res.status(500).json({ message: 'Internal server error' })
+    console.log(`login post error: ${err}`);
+    res.status(500).json({ message: 'Internal server error' });
   }
-}
+};
 
 const logout = (req, res) => {
   try {
