ebba153298f8c779925dc75380aa9ab35137e2af
Abdelrahman Elshreif <link93955@gmail.com>
2024-08-14 12:44:58 +0300

    edit cors

--- server/app.js
+++ server/app.js
@@ -72,7 +72,7 @@ const app = express();
 
 // Middlewares
 app.use(cors({
-  origin: ['http://localhost:5173','https://https://odeaura-api.vercel.app'], // or '*' to allow all origins
+  origin: ['http://localhost:5173','https://odeaura.vercel.app/'], // or '*' to allow all origins
   credentials: true, // Allow cookies to be sent with requests
   allowedHeaders: ['Content-Type', 'Authorization'], // Allow these headers
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow these methods
