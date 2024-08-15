f2191c820e54139e2f1ac8e4023535d47e688f46
Abdelrahman Elshreif <link93955@gmail.com>
2024-08-15 06:28:39 +0300

    login base fetch

--- client/src/api/AuthAPI.js
+++ client/src/api/AuthAPI.js
@@ -70,7 +70,7 @@ const authAPI = {
 
   login: async ({ email, password }) => {
     try {
-      const response = await fetch('http://localhost:3000/login', {
+      const response = await fetch('https://odeaura.vercel.app/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
