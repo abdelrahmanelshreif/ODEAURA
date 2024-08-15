1ca0993f9cf961ed540e9ff12c903272becf14d5
Abdelrahman Elshreif <link93955@gmail.com>
2024-08-15 23:59:12 +0300

    testX

--- client/src/api/AuthAPI.js
+++ client/src/api/AuthAPI.js
@@ -70,14 +70,12 @@ const authAPI = {
 
   login: async ({ email, password }) => {
     try {
-      const response = await fetch('https://odeaura-api.vercel.app/login', {
-      // const response = await fetch('http://localhost:3000/login', {
+      const response = await fetch('https://odeaura.vercel.app/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
           'Accept': 'application/json, text/plain, */*',
           'Origin': 'https://odeaura.vercel.app',
-          // 'Origin': 'http://localhost:5173',
         },
         body: JSON.stringify({ email, password }),
         credentials: 'include', // Important to include cookies
@@ -133,4 +131,3 @@ const authAPI = {
 };
 
 export default authAPI;
-
