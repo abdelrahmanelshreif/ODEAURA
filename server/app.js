3a3ae13144a8be2d881458bb08e7d7ce69014068
Abdelrahman Elshreif <link93955@gmail.com>
2024-08-16 06:59:13 +0300

    reverse

--- server/app.js
+++ server/app.js
@@ -137,22 +137,22 @@ const app = express();
 
 
 
-// CORS Configuration
-// const corsOptions = {
-//   origin: ['https://odeaura.vercel.app', 'http://localhost:5173'], // Allow frontend origins
-//   methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'], // Allow necessary methods
-//   credentials: true, // Allow cookies and credentials
-//   allowedHeaders: ['X-CSRF-Token', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'] // Allow necessary headers
-// };
+CORS Configuration
+const corsOptions = {
+  origin: ['https://odeaura.vercel.app', 'http://localhost:5173'], // Allow frontend origins
+  methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'], // Allow necessary methods
+  credentials: true, // Allow cookies and credentials
+  allowedHeaders: ['X-CSRF-Token', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization','jwt','token'] // Allow necessary headers
+};
 
-// // Apply CORS middleware
-// app.use(cors(corsOptions));
+// Apply CORS middleware
+app.use(cors(corsOptions));
 
-// // Handle preflight OPTIONS requests for all routes
-// app.options('*', cors(corsOptions));
+// Handle preflight OPTIONS requests for all routes
+app.options('*', cors(corsOptions));
 
 
-app.use(cors());
+
 
 
 app.use(express.json());
