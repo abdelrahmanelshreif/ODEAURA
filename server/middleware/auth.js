21ca5f07226969477ff5c5ed75cd7ae0011eb968
Abdelrahman Elshreif <link93955@gmail.com>
2024-08-16 22:39:52 +0300

    clg jwt

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -33,6 +33,7 @@ const isAuthenticated = (req, res, next) => {
   try {
     isTokenFound(token)
     const decodedToken = jwt.verify(token, process.env.SECRET_KEY)
+    console.log(token);
     next()
   } catch (err) {
     if (err.message === 'not authenticated') {
