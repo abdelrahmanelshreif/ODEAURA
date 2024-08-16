619317ebf13f431eb094f27e0038ff2789292ee8
Abdelrahman Elshreif <link93955@gmail.com>
2024-08-16 07:35:11 +0300

    /me 8

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken')
 
 const getMe = (req, res) => {
   // Access cookies directly
-  const token = req.cookies.token; // Assuming the token is stored in a cookie named 'token'
+  const token = req.cookies.jwt; // Assuming the token is stored in a cookie named 'token'
 
   if (!token) {
     return res.status(401).json({ message: 'No token provided' });
