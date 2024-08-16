f318baefa4b3dc74b750e51394d8707c306aa5d2
Abdelrahman Elshreif <link93955@gmail.com>
2024-08-16 22:29:38 +0300

    new rollback to /me

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -8,9 +8,10 @@ const getToken = () => {
   // Example using cookies
   const matches = document.cookie.match(new RegExp('(^| )jwt=([^;]+)'));
   if (matches) return matches[2];
+  
+  // Alternatively, use local storage
+  // return localStorage.getItem('token');
 };
-
-
 
 // Function to set the token in cookies or local storage
 const setToken = (token) => {
@@ -57,4 +58,4 @@ axiosClient.interceptors.response.use(
     throw err.response;
   }
 );
-export default (axiosClient,getToken);
+export default axiosClient;
