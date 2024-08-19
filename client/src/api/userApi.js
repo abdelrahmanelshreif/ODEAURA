69bc7639b64438d710ec3889e0605e77fe4f3cf1
Abdelrahman Elshreif <link93955@gmail.com>
2024-08-19 07:55:19 +0300

    test signup

--- client/src/api/userApi.js
+++ client/src/api/userApi.js
@@ -6,32 +6,3 @@ const UserApi = {
 
 export default UserApi
 
-// import getToken from './AuthAPI'
-// const baseUrl = 'https://odeaura-api.vercel.app/';
-
-// const fetchWithAuth = async (url, options = {}) => {
-//   const token = getToken();
-//   if (token) {
-//     options.headers = {
-//       ...options.headers,
-//       'Authorization': `Bearer ${token}`
-//     };
-//   }
-//   const response = await fetch(`${baseUrl}${url}`, options);
-//   if (!response.ok) {
-//     throw new Error(`HTTP error! Status: ${response.status}`);
-//   }
-//   return response.json();
-// };
-
-// const userAPI = {
-//   updateMe: (params) => fetchWithAuth('updateMe', {
-//     method: 'PUT',
-//     headers: {
-//       'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify(params)
-//   }),
-// };
-
-// export default userAPI;
